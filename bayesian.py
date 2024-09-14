f740513cdb63565de0f1cb0cc6bf85b9d628e518
Hamanda Cavalheri <hamandacavalheri@Hamandas-MacBook-Pro.local>
2024-09-14 19:20:05 -0300

    commentsgit push :Q i

--- bayesian.py
+++ bayesian.py
@@ -10,19 +10,13 @@ true_intercept = 1.0
 noise = np.random.normal(scale=1.0, size=n_samples)
 y = true_slope * X + true_intercept + noise
 
-
-# Define the Bayesian model
 with pm.Model() as model:
-    # Priors for unknown model parameters
     slope = pm.Normal("slope", mu=0, sigma=10)
     intercept = pm.Normal("intercept", mu=0, sigma=10)
     sigma = pm.HalfNormal("sigma", sigma=1)
 
-    # Expected value of outcome
     mu = slope * X + intercept
 
-    # Likelihood (sampling distribution) of observations
     y_obs = pm.Normal("y_obs", mu=mu, sigma=sigma, observed=y)
 
-    # Perform Bayesian inference
     trace = pm.sample(2000, return_inferencedata=False)
